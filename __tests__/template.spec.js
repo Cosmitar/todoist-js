29d064e2310bf7776e56037c5bd97dae1f3d7d05
Cosmitar <mhherrera31@gmail.com>
2017-05-11 00:21:06 -0300

    write test for project template: export as file

--- __tests__/template.spec.js
+++ __tests__/template.spec.js
@@ -7,17 +7,33 @@ jasmine.DEFAULT_TIMEOUT_INTERVAL = 10000;
 import API from './../todoist/Api';
 const api = new API(process.env.ACCESS_TOKEN);
 
-test('Manager should export project as url', async () => {
+let item1;
+let project1;
+
+afterAll(async () => {
+  item1.delete();
+  project1.delete();
+  await api.commit();
+});
+
+test('Manager should export project as file', async () => {
   await api.sync();
-  const project1 = api.projects.add('Project1_template');
-  const project2 = api.projects.add('Project2_template');
+  project1 = api.projects.add('Project1_template');
   await api.commit();
 
-  const item1 = api.items.add('Item1_template', project1.id);
+  item1 = api.items.add('Item1_template', project1.id);
   await api.commit();
+
+  const template_file = await api.templates.export_as_file(project1.id);
+  const fileContent = await template_file.text();
+
+  expect(fileContent).toEqual(expect.stringMatching(/task,Item1_template,4,1,/));
+});
+
+test('Manager should export project as url', async () => {
+  // requires project1 and item1
 
   const template_url = await api.templates.export_as_url(project1.id);
-
   // validates returned object structure and data
   expect(template_url).toHaveProperty('file_name', expect.stringMatching(/_Project1_template\.csv$/));
   expect(template_url).toHaveProperty('file_url', expect.stringMatching(/(http(s?):)|([\/|.|\w|\s])*_Project1_template\.(?:csv)/));
@@ -37,9 +53,4 @@ test('Manager should export project as url', async () => {
   const fileResponse = await getFile();
 
   expect(fileResponse).toEqual(expect.stringMatching(/task,Item1_template,4,1,/));
-
-  item1.delete();
-  project1.delete();
-  project2.delete();
-  await api.commit();
 });
